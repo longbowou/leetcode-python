bb1c56de2bf15ed2f60aedfd82768f8e1d31d345
Longbowou <blandedaniel@gmail.com>
2025-02-27 11:14:59 -0600

    Solve 219. Contains Duplicate II

---
+++ 219-contains-duplicate-ii.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/contains-duplicate-ii/description/
+# 219. Contains Duplicate II
+# Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: nums = [1,2,3,1], k = 3
+# Output: true
+# Example 2:
+# 
+# Input: nums = [1,0,1,1], k = 1
+# Output: true
+# Example 3:
+# 
+# Input: nums = [1,2,3,1,2,3], k = 2
+# Output: false
+#  
+# 
+# Constraints:
+# 
+# 1 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# 0 <= k <= 105
+#
+# Time Complexity O(n)
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window = set()
+
+        l = 0
+        for r in range(len(nums)):
+            if r - l > k:
+                window.remove(nums[l])
+                l += 1
+
+            if nums[r] in window:
+                return True
+
+            window.add(nums[r])
+
+        return False
